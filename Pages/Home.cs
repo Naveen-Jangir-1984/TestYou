4e4b3ab2da03f48eb2cb55ddc8fdb5962811f13c
Naveen Kumar Jangir <206243@cognizant.com>
2023-01-16 12:04:36 +0530

    NJ#stay-signed-in-logic

--- Pages/Home.cs
+++ Pages/Home.cs
@@ -16,6 +16,7 @@ namespace TestYou.Pages
         private readonly By Txt_Username = By.XPath("(//input[contains(@id,'_txtUserLogin')])[2]");
         private readonly By Txt_Password = By.XPath("(//input[contains(@id,'_txtPassword')])[2]");
         private readonly By Btn_Login = By.XPath("//input[contains(@id,'_btnLogin')]");
+        private readonly By Btn_StaySignedIn = By.XPath("(//input[contains(@id,'RememberMe')])[2]");
         public Home(WebDriver driver)
         {
             this.Driver = driver;
